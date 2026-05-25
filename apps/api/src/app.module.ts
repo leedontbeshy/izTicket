24c66f9d32b623aafc9c8012df18cf7d2be6a6a9
leedontbeshy <duyphucle.0510@gmail.com>
2026-05-25 13:45:02 +0700

    Implement authentication module with controller, service, and DTOs; add tests and update package dependencies

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import { validateEnv } from './config/env.validate';
 import { CommonModule } from './common/common.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
     imports: [
@@ -15,6 +16,7 @@ import { CommonModule } from './common/common.module';
         }),
         CommonModule,
         HealthModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],
